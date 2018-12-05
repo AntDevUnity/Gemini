273c2a9ee64bd8dddcf7a875467c4e42a197a9a5
Antony Robert Wells <antonyrobwells@gmail.com>
2018-12-05 23:52:59 +0000

    Initial commit.

---
+++ Source/Gemini/GeminiGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GeminiGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGeminiGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGeminiGameMode();
+};
+
+
+
